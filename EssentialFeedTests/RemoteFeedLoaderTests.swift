9529b3630f0752738f7355ab513ecc02a9d7cdf5
Emese <emese.toth.uk@gmail.com>
2026-08-23 12:22:48 +0100

    replace optional params with enum w success/failure

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -45,21 +45,21 @@ class RemoteFeedLoaderTests: XCTestCase {
         let clientError =  NSError(domain: "Test", code: 0)
         client.complete(with: clientError)
         
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        XCTAssertEqual(capturedErrors, [.connectivity])
         
     }
     
     func test_load_deliversErrorOnNon200HTTPSResponse() {
         let (sut, client) = makeSUT()
+        
         let sampleErrorCodes = [199,201, 300, 400, 500].enumerated()
+        
         sampleErrorCodes.forEach { index, code in
             var capturedErrors = [RemoteFeedLoader.Error]()
             sut.load { capturedErrors.append($0) }
             
             client.complete(withStatusCode: code, at: index)
-            XCTAssertEqual(capturedErrors, [.connectivity])
-
-            capturedErrors = []
+            XCTAssertEqual(capturedErrors, [.invalidData])
         }
     }
 
@@ -72,18 +72,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -91,8 +91,8 @@ class RemoteFeedLoaderTests: XCTestCase {
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 
