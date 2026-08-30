163d911ff71af2013fcf355eca560ddff9232b4a
Emese <emese.toth.uk@gmail.com>
2026-08-30 10:10:28 +0100

    delivers invalid data on 200

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -52,6 +52,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPSResponse() {
         let (sut, client) = makeSUT()
         
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data("invalid json".utf8)
+        
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
+    func test_load_deliversErrornOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
         let sampleErrorCodes = [199,201, 300, 400, 500].enumerated()
         
         sampleErrorCodes.forEach { index, code in
@@ -86,13 +98,13 @@ class RemoteFeedLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
-            let response = HTTPURLResponse(url: requestedURLs[index], 
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
+            let response = HTTPURLResponse(url: requestedURLs[index],
                                            statusCode: code,
                                            httpVersion: nil,
                                            headerFields: nil
             )!
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data,response))
         }
     }
 
